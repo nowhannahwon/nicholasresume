de8e32c7e30a88f4649f3204a9594d3198bb6014
Hannah Won <nowhananhwon@gmail.com>
2014-09-03 20:06:13 -0700

    Initial commmit

---
+++ index.js
@@ -0,0 +1,19 @@
+alert("JS loaded!");
+
+var Nicholas = {
+  firstName: "Nicklaus",
+  midName: "Urnndy",
+  lastName: "Karrey",
+  email: "niklass@geemail.kam",
+  phone: "356-564-5565",
+  gitHub: "http://github.com/blahblah",
+  linkedIn: "www.linkedin.com/blah",
+  address: "666 Love Lane Somewhere, BL 54654",
+
+  // displayName = function() {
+  //   $("h1").textContent(Nicholas.firstName + " " + Nicholas.midName + " " + Nicholas.lastName);
+  // },
+
+}
+
+
