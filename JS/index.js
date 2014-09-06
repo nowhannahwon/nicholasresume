61066d89b0f756f5489fbae3b51e77edf8e5a326
Hannah Won <nowhananhwon@gmail.com>
2014-09-05 23:49:46 -0700

    links up jQuery file and object

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,20 @@
-alert("JS loaded!");
+// alert("JS loaded!");
 
-var Nicholas = {
+var Person = {
   firstName: "Nicklaus",
   midName: "Urnndy",
   lastName: "Karrey",
+  fullName: this.firstName + " " + this.midName + " " + this.lastName,
   email: "niklass@geemail.kam",
   phone: "356-564-5565",
   gitHub: "http://github.com/blahblah",
   linkedIn: "www.linkedin.com/blah",
   address: "666 Love Lane Somewhere, BL 54654",
 
-  // displayName = function() {
-  //   $("h1").textContent(Nicholas.firstName + " " + Nicholas.midName + " " + Nicholas.lastName);
-  // },
+  displayName: function() {
+    $("#mainheader").text(this.fullName);
+  }
+};
 
-}
-
+$(document).ready(Person.displayName);
 
